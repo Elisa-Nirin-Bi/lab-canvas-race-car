8da3088d79a94445fc0fcee0da5e30998ae5cdaf
Elisa Barontini <nirinproductions@gmail.com>
2021-07-05 16:39:10 +0100

    iteractions 1 and 2 completed

--- js/index.js
+++ js/index.js
@@ -1,7 +1,33 @@
+const canvas = document.querySelector('canvas');
+const context = canvas.getContext('2d');
+
+
 window.onload = () => {
   document.getElementById('start-button').onclick = () => {
     startGame();
   };
+function closeStartPage(){
+  const close = document.getElementById("intro");
+  close.style.display = "none"; 
+}
+ 
+  function startGame() {
+    closeStartPage()
+    const roadImg = new Image();
+  roadImg.src = './images/road.png';
+  roadImg.addEventListener('load', () => {
+    context.drawImage(roadImg, 0, 0, 500,700);
+    
+  })
+  
 
-  function startGame() {}
-};
+ 
+  const carImg = new Image();
+  carImg.src = './images/car.png';
+  carImg.addEventListener('load', () => {
+   
+      context.drawImage(carImg, 220, 580, 50,100);
+      
+    })
+  
+  }}
